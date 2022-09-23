f6fe998a1c3bb2c56fcf038d4b72f334b9154705
JotiradityaB <114105421+JotiradityaB@users.noreply.github.com>
2022-09-23 18:55:01 +0530

    Create to-lower-case.cpp

---
+++ to-lower-case.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/to-lower-case/
+
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (char& c : str) {
+        if (c >= 'A' && c <= 'Z') c += 32;
+    }
+    return str;
+    }
+};
