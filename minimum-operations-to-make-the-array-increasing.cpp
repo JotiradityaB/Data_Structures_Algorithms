c9ea8d10856396485ce0c1feea35dbf0ba7aec37
JotiradityaB <114105421+JotiradityaB@users.noreply.github.com>
2022-10-05 20:59:21 +0530

    Create minimum-operations-to-make-the-array-increasing.cpp

---
+++ minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int output=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<nums[i+1])
+                continue;
+            else{
+                output=output+(nums[i]+1-nums[i+1]);
+                nums[i+1]=nums[i]+1;
+            }
+        }
+        return output;
+    }
+};
